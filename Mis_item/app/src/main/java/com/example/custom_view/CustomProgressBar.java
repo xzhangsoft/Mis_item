689fc02c646849ae688ae657d41aebfe03a06023
dany198x <dany198x@163.com>
2019-05-21 15:26:11 +0800

    更新constructor resource的例子

--- Mis_item/app/src/main/java/com/example/custom_view/CustomProgressBar.java
+++ Mis_item/app/src/main/java/com/example/custom_view/CustomProgressBar.java
@@ -1,11 +1,14 @@
 package com.example.custom_view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -17,6 +20,7 @@ public class CustomProgressBar extends View {
     private int mFirstColor;
     private int mCircleWidth;
     private int mCircleAngle;
+    private String mCircleString;
     private Paint mPaint;
 
     private boolean isFirst = true;
@@ -26,24 +30,29 @@ public class CustomProgressBar extends View {
     }
 
     public CustomProgressBar(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        this(context, attrs, R.attr.StyleInTheme);
     }
 
-    public CustomProgressBar(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
-                R.styleable.CustomProgressBar, defStyle, 0);
+    public CustomProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, R.style.StyleForDefStyleRes);
 
+//        new Thread(this).start();
+    }
+
+    public CustomProgressBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.CustomProgressBar, defStyleAttr, defStyleRes);
+//        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+//                R.styleable.CustomProgressBar, 0, defStyleRes);
         mFirstColor = a.getColor(R.styleable.CustomProgressBar_circle_color,
                 DEFAULT_BORDER_COLOR);
         mCircleWidth = a.getDimensionPixelSize(
                 R.styleable.CustomProgressBar_circle_width, 10);
         mCircleAngle = a.getInt(
                 R.styleable.CustomProgressBar_circle_angle, 360);
+        mCircleString = a.getString(R.styleable.CustomProgressBar_circle_text);
         a.recycle();
-
-
-//        new Thread(this).start();
     }
 
     private void init() {
@@ -63,6 +72,13 @@ public class CustomProgressBar extends View {
         mPaint.setColor(mFirstColor);
 //            canvas.drawCircle(center, center, radius, mPaint);
         canvas.drawArc(oval, 0, mCircleAngle, false, mPaint);
+        Path path = new Path();
+        path.moveTo(0,0);
+        path.lineTo(100,100);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextSize(40);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawTextOnPath(mCircleString, path,10,10, mPaint);
     }
 }
 
